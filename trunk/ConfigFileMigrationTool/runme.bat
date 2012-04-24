78403896cc160cf358f83e188c53061fd697a41d
marcoratto <marcoratto@55cbd778-2c59-11df-8b4d-31f007950f82>
2012-04-24 10:59:56 +0000

    Initial import.

---
+++ trunk/ConfigFileMigrationTool/runme.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal
+set JAVA_OPTS=-Xms64m -Xmx1024m
+
+set PRGDIR=%CD%
+
+set CONFIG_DIR=%USERPROFILE%\.MQJExplorer
+IF not EXIST "%CONFIG_DIR%" (
+	mkdir "%CONFIG_DIR%"
+)
+
+set LOG_DIR=%CONFIG_DIR%\log
+IF not EXIST "%LOG_DIR%" (
+	mkdir "%LOG_DIR%"
+)
+
+SET CPATH=%PRGDIR%\bin\ConfigFileMigrationTool.jar
+SET CPATH=%PRGDIR%\res;%CPATH%
+
+: checkJava
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+if "%JAVACMD%" == "" set JAVACMD=%JAVA_HOME%\bin\java.exe
+goto runMQJExplorer
+
+: noJavaHome
+if "%JAVACMD%" == "" set JAVACMD=java.exe
+
+: runMQJExplorer
+"%JAVACMD%" %JAVA_OPTS% -Dlog4j.debug -classpath "%CPATH%" com.kolban.mqjexplorer.ConfigFileMigrationTool
+if errorlevel == 1 pause
+endlocal
---
+++ trunk/ConfigFileMigrationTool/runme.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#set -x
+
+JAVA_OPTS="-Xms256m -Xmx1024m"
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+
+LIB_DIR=$PRGDIR/lib
+
+CPATH=$PRGDIR/bin/ConfigFileMigrationTool.jar
+for JAR_FILE in `find "$LIB_DIR" -type f -name "*.jar"`
+do
+CPATH=$JAR_FILE:$CPATH
+done
+
+if [ -z "$JAVACMD" ] ; then
+  if [ -n "$JAVA_HOME"  ] ; then
+    # IBM's JDK on AIX uses strange locations for the executables
+    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+      JAVACMD="$JAVA_HOME/jre/sh/java"
+    elif [ -x "$JAVA_HOME/jre/bin/java" ] ; then
+      JAVACMD="$JAVA_HOME/jre/bin/java"
+    else
+      JAVACMD="$JAVA_HOME/bin/java"
+    fi
+  else
+    JAVACMD=`which java 2> /dev/null `
+    if [ -z "$JAVACMD" ] ; then
+        JAVACMD=java
+    fi
+  fi
+fi
+
+if [ ! -x "$JAVACMD" ] ; then
+  echo "Error: JAVA_HOME is not defined correctly."
+  echo "I cannot execute $JAVACMD"
+  exit 1
+fi
+
+"$JAVACMD" $JAVA_OPTS -classpath $CPATH -Dlog4j.debug com.kolban.mqjexplorer.ConfigFileMigrationTool
+RET_CODE=$?
+if [ $RET_CODE -ne 0 ]
+then
+        echo "ERROR! java return error code $RET_CODE."
+        exit 1
+fi
+exit 0
