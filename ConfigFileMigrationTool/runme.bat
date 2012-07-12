8e19c9d604284cc8706ee4f10a1cb71f96339ef6
marcoratto <marcoratto@55cbd778-2c59-11df-8b4d-31f007950f82>
2012-07-12 13:19:21 +0000

    Release 0.1

--- ConfigFileMigrationTool/runme.bat
+++ ConfigFileMigrationTool/runme.bat
@@ -15,6 +15,21 @@ IF not EXIST "%LOG_DIR%" (
 )
 
 SET CPATH=%PRGDIR%\bin\ConfigFileMigrationTool.jar
+SET CPATH=%PRGDIR%\lib\com.ibm.mq.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\com.ibm.mqjms.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\com.ibm.mq.pcf-6.0.3.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\com.ibm.mq.pcf.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\connector.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\fscontext.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\jhbasic.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\jms.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\jndi.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\jta.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\ldap.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\MQJExplorer.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\mqjhelp.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\providerutil.jar;%CPATH%
+SET CPATH=%PRGDIR%\lib\xerces.jar;%CPATH%
 SET CPATH=%PRGDIR%\res;%CPATH%
 
 : checkJava
